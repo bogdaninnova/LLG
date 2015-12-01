a77116d0dd4833e377e245bc99448287bf46ba78
Bohdan Pedchenko <bogdan.innova@gmail.com>
2015-12-01 14:52:58 +0200

    Update Launcher.java

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,23 +1,19 @@
 package main;
 
-import java.util.*;
-
-import painting.Draw;
-
-import painting.CreateGIF;
-import painting.Draw;
-import painting.DrawComponents;
-
 import main.fields.Anisotrophia;
 import main.fields.Circular;
-import main.fields.Impuls;
+import painting.Draw;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Launcher {
 
 	public static void main(String...strings) {
 		//longStory();
-		//System.out.println("test");
-		oneParticle(0.25, 0.1, 1.0);
+		System.out.println("test");
+		//oneParticle(0.0, 0.1, 0.5);
 	}
 
 
