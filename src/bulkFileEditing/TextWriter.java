98f2834e41b9a1543ef3009c93a51753a4e11ea5
bope0915 <bogdan.innova@gmail.com>
2015-12-22 15:34:33 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/Launcher.java

---
+++ src/bulkFileEditing/TextWriter.java
@@ -0,0 +1,80 @@
+package bulkFileEditing;
+
+import main.Vector;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class TextWriter {
+
+    @SuppressWarnings("resource")//damn
+    public static void writeDoubleList(List<Double> averrageList, String name) {
+
+        ListIterator<Double> iter = averrageList.listIterator();
+
+        File file = new File(name + ".txt");
+        try {
+            FileWriter writer = new FileWriter(file);
+
+            while (iter.hasNext())
+                writer.append(iter.next() + "\r\n");
+
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("resource")
+    public static void writeIntegerList(LinkedList<Integer> averrageList, String name) {
+
+        ListIterator<Integer> iter = averrageList.listIterator();
+
+        File file = new File(name + ".txt");
+        try {
+            FileWriter writer = new FileWriter(file);
+
+            while (iter.hasNext()) writer.append(iter.next() + "\r\n");
+
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("resource")
+    public static void writeTraectorysCoordinates(List<Vector> list, String name) {
+
+        //String tittle = name + new SimpleDateFormat("yyyy-MM-dd HH-mm-ss.SSS").format(new Date());
+
+        File xFile = new File(name + "_x.txt");
+        File yFile = new File(name + "_y.txt");
+        File zFile = new File(name + "_z.txt");
+
+        ListIterator<Vector> iter = list.listIterator();
+
+        try {
+            FileWriter xWriter = new FileWriter(xFile);
+            FileWriter yWriter = new FileWriter(yFile);
+            FileWriter zWriter = new FileWriter(zFile);
+
+            Vector dot;
+
+            while (iter.hasNext()){
+                dot = iter.next();
+                xWriter.append(dot.getX() + "\r\n");
+                yWriter.append(dot.getY() + "\r\n");
+                zWriter.append(dot.getZ() + "\r\n");
+            }
+            xWriter.flush();
+            yWriter.flush();
+            zWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
